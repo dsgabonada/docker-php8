ff850c1e25698627bd5a39ea5e74d1adfbd9e2dc
dsgabonada <dsanchezgabonada@gmail.com>
2024-08-16 23:28:56 +0000

    commit

--- public_html/ci4/app/Config/Routes.php
+++ public_html/ci4/app/Config/Routes.php
@@ -6,3 +6,14 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+use App\Controllers\News;
+use App\Controllers\Pages;
+
+$routes->get('pages', [Pages::class, 'index']);
+$routes->get('news/new', [News::class, 'new']);
+$routes->post('news', [News::class, 'create']);
+$routes->get('news/(:segment)', [News::class, 'show']);
+
+$routes->get('pages', [Pages::class, 'index']);
+$routes->get('(:segment)', [Pages::class, 'view']);
