508574db27476cc3cf410caa7e634dedd46faae5
dsgabonada <dsanchezgabonada@gmail.com>
2024-08-16 14:39:24 +0000

    commit

--- public_html/database02.php
+++ public_html/database02.php
@@ -12,6 +12,7 @@ if (!$conn) {
 }
 
 // sql to create table
+/*
 $sql = "CREATE TABLE MyGuests (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 firstname VARCHAR(30) NOT NULL,
@@ -19,12 +20,16 @@ lastname VARCHAR(30) NOT NULL,
 email VARCHAR(50),
 reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )";
+*/
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('Jane', 'Doe', 'jane@example.com')";
 
 if (mysqli_query($conn, $sql)) {
-  echo "Table MyGuests created successfully";
-} else {
-  echo "Error creating table: " . mysqli_error($conn);
-}
-
-mysqli_close($conn);
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+  mysqli_close($conn);
 ?> 
--- public_html/index.php
+++ public_html/index.php
@@ -1,16 +1,5 @@
 <?php
 
- $servername = "mysql";
- $username = "mysql";
- $password = "mysql";
- 
- // Create connection
- $conn = new mysqli($servername, $username, $password);
- 
- // Check connection
- if ($conn->connect_error) {
-   die("Connection failed: " . $conn->connect_error);
- }
- echo "Connected successfully";
+phpinfo();
 
 ?>
